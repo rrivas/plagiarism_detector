66be63d7f18c15a4eb0e950aa2a20975e9936e43
Rob Rivas <robertrivas@gmail.com>
2018-09-14 16:47:49 -0700

    MVP. Now make it pretty

--- detector.py
+++ detector.py
@@ -4,9 +4,10 @@ import string
 syns_file = open(sys.argv[1], 'r').read()
 # maybe do the punctuation thing?
 # purpase was an attempt to handle exclamation points
+# lower case all the things
 file_1 = ' '.join(open(sys.argv[2], 'r').read().split()).translate(None, string.punctuation)
 file_2 = open(sys.argv[3], 'r').read()
-num_tuples = int(sys.argv[4])
+num_tuples = int(sys.argv[4]) # default to three if blank
 
 def synonym_dictionary(synonyms_file):
     dictionary = {}
@@ -15,7 +16,7 @@ def synonym_dictionary(synonyms_file):
     for row in synonym_array:
         synonyms = row.split()
         sorted_synonyms = sorted(synonyms, key=str.lower)
-        hashed_synonyms_value = ''.join([str(x) for x in sorted_synonyms])
+        hashed_synonyms_value = ''.join(sorted_synonyms)
 
         for synonym in synonyms:
             dictionary[synonym] = hashed_synonyms_value
@@ -30,7 +31,8 @@ def tuples(words, tuple_size):
     for idx, word in enumerate(words):
         if max_index >=  idx:
             tuple = words[idx:idx+tuple_size]
-            tuples.append(tuple)
+            joined_tuple = ' '.join(tuple)
+            tuples.append(joined_tuple)
 
     return tuples
 
@@ -46,6 +48,11 @@ def synonymize(input_file, dictionary):
 
     return synonymized_file
 
+def intersection(file_1_tuples, file_2_tuples):
+    file_2_set = set(file_2_tuples)
+    intersection_list = [value for value in file_1_tuples if value in file_2_set]
+    return intersection_list
+
 dictionary = synonym_dictionary(syns_file)
 file_1_words = synonymize(file_1, dictionary)
 file_2_words = synonymize(file_2, dictionary)
@@ -53,8 +60,22 @@ file_2_words = synonymize(file_2, dictionary)
 file_1_tuples = tuples(file_1_words, num_tuples)
 file_2_tuples = tuples(file_2_words, num_tuples)
 
-print dictionary
+# print dictionary
+# print file_1_tuples
+# print file_2_tuples
 
-print file_1_tuples
-print file_2_tuples
+intersecting_tuples = intersection(file_1_tuples, file_2_tuples)
+rate_of_intersection = float(len(intersecting_tuples)) / len(file_2_tuples)
+print "Plagirism Rate: {0:.0%}".format(rate_of_intersection)
+
+# Base file to run as a main
+# - Outputs instructions if no args passed in
+# - Deals with bad args
+# - Adds default to num_tuples
+# Dictionary object
+# - Passed aound to find keys
+# Tuples/Words Object
+# - Place where the document information is stored
+# Plagirism Calculator Object
+# - Object that calculates the intersection and the rate
 
