6cf244673412e0c70bc8648ae0b522b4dbd1d422
Rob Rivas <robertrivas@gmail.com>
2018-09-14 23:42:38 -0700

    Add description to argument parser

--- detector.py
+++ detector.py
@@ -1,27 +1,25 @@
 import sys
+import argparse
 
 from synonym import Synonym
 from file import File
 from calculate_intersection import CalculateIntersection
 
-tuple_size = 3
 args = sys.argv[1:]
 
-if len(args) < 3:
-    print "Instructions"
-    sys.exit()
-elif len(args) == 4:
-    tuple_size = int(args[3])
-elif len(args) > 4:
-    print "Instructions"
-    sys.exit()
+parser = argparse.ArgumentParser(description='Outputs a rate of plagiarizations based on the number of N-tuples in the comparison file that appear in the control file, where the tuples are compared by accounting for synonyms provided in the synonym file.')
+parser.add_argument('-s','--synonym_file', help='Synonym file path', required=True)
+parser.add_argument('-c','--comparison_file', help='Comparison file path', required=True)
+parser.add_argument('-o','--control_file', help='Control file path', required=True)
+parser.add_argument('-t','--tuple_size', help='Size of comparison tuple',  default=3, required=False)
+args = parser.parse_args()
 
-synonym_file = args[0]
-control_file = args[1]
-comparison_file = args[2]
+tuple_size = int(args.tuple_size)
 
-dictionary = Synonym(synonym_file).dictionary()
-control = File(control_file, dictionary, tuple_size)
-comparison = File(comparison_file, dictionary, tuple_size)
+dictionary = Synonym(args.synonym_file).dictionary()
+control = File(args.control_file, dictionary, tuple_size)
+comparison = File(args.comparison_file, dictionary, tuple_size)
 
+print control.synonymized_tuples
+print comparison.synonymized_tuples
 print CalculateIntersection(control.synonymized_tuples, comparison.synonymized_tuples).rate_text()
